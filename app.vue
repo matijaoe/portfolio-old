d077e9dcc6ec17ae19b8541d8a8bd678ce89e6f9
Matija Osrečki <matija.osrecki@gmail.com>
2022-10-25 22:59:12 +0200

    Add page transitions

--- app.vue
+++ app.vue
@@ -26,4 +26,26 @@ useHead({
   background-attachment: fixed;
   backdrop-filter: grayscale(65%);
 }
+
+.page-enter-active {
+  transition: all 100ms ease-out;
+}
+
+.page-leave-active {
+  transition: all 150ms cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.page-enter-from {
+  transform: translateY(-1rem);
+  opacity: 0;
+}
+.page-leave-to {
+  transform: translateY(1rem);
+  opacity: 0;
+}
+
+::selection {
+  background: #d4d4d4;
+  color: #292524;
+}
 </style>
