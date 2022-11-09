17833adc4b376417c904464483fb7eb51005bd39
Matija Osrečki <matija.osrecki@gmail.com>
2022-11-09 11:27:06 +0100

    Remove plausible nuxt 3 module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,21 +9,11 @@ export default defineNuxtConfig({
     '@nuxt/content',
     '@nuxt/image-edge',
     '@nuxtjs/color-mode',
-    '@huntersofbook/plausible-nuxt',
     // '@nuxtjs/fontaine',
   ],
   experimental: {
     reactivityTransform: true,
     inlineSSRStyles: false,
-  },
-  plausible: {
-    init: {
-      domain: 'localhost',
-      apiHost: 'https://site.com',
-      trackLocalhost: true,
-    },
-    // If this is loaded you can make it true, https://github.com/nuxt-modules/partytown
-    partytown: false,
   },
   unocss: {
     preflight: true,
