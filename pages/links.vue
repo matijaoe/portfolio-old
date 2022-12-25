f4c7eff34918d09635586c6d353fe3b2e85865db
Matija Osrečki <matija.osrecki@gmail.com>
2022-12-25 02:17:19 +0100

    Add art to links

--- pages/links.vue
+++ pages/links.vue
@@ -8,17 +8,52 @@ definePageMeta({
 })
 
 const { shortcutsShown } = useSocialsShortcuts()
+
+const bgImages = [
+  'https://mdl.artvee.com/sftb/403901mt.jpg',
+  'https://mdl.artvee.com/sftb/402633mt.jpg',
+  'https://mdl.artvee.com/sftb/403588mt.jpg',
+  'https://mdl.artvee.com/sftb/101906ab.jpg',
+  'https://mdl.artvee.com/sftb/225614fg.jpg',
+  'https://mdl.artvee.com/sftb/103885ab.jpg',
+  'https://mdl.artvee.com/sftb/401074mt.jpg',
+  'https://mdl.artvee.com/sftb/700854an.jpg',
+  'https://mdl.artvee.com/sftb/501118ld.jpg',
+  'https://mdl.artvee.com/sftb/600248sl.jpg',
+  'https://mdl.artvee.com/sftb/401288mt.jpg',
+  'https://mdl.artvee.com/sftb/515596ld.jpg',
+
+]
 </script>
 
 <template>
   <div max-w-8xl mx-auto flex-1 flex flex-col w-full gap-20>
     <div class="link-grid" gap-4>
+      <div
+        v-for="img in bgImages.slice(0, 6)"
+        :key="img"
+        rounded-2xl
+        select-none
+        overflow-hidden
+        class="col-span-3 sm:(col-span-2 row-span-1) aspect-2/1 sm:aspect-square filter-saturate-90"
+        :style="{ backgroundImage: 'url(' + `'${img}'` + ')', backgroundSize: 'cover' }"
+      />
       <LinksLinkCard
         v-for="(social, idx) in socials"
         :key="social.label"
         :social="social"
         :idx="idx"
         :show-key="shortcutsShown"
+      />
+
+      <div
+        v-for="img in bgImages.slice(6)"
+        :key="img"
+        rounded-2xl
+        select-none
+        overflow-hidden
+        class="col-span-3 sm:(col-span-2 row-span-1) aspect-2/1 sm:aspect-square filter-saturate-90"
+        :style="{ backgroundImage: 'url(' + `'${img}'` + ')', backgroundSize: 'cover', backgroundPosition: 'center' }"
       />
     </div>
     <div flex justify-center sm:justify-end mt-auto pb-12 sm:pb-0>
