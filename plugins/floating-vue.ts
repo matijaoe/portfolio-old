13bb49d6bd4f4ba993a6be1e772a4ad079db62bb
Matija Osrečki <matija.osrecki@gmail.com>
2022-12-26 18:38:25 +0100

    Add a whole bunch of js snippets

--- plugins/floating-vue.ts
+++ plugins/floating-vue.ts
@@ -1,7 +1,7 @@
 import FloatingVue from 'floating-vue'
 import 'floating-vue/dist/style.css'
 
-export default defineNuxtPlugin((nuxtApp) => {
+  export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(FloatingVue, {
     themes: {
       'info-tooltip': {
